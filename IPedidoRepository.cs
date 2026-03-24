80eb3eb8dbc1cd4006d9a3849ff7bfc9e8783e4f
Edgard <edgardpimentaa02@gmail.com>
2026-03-24 12:59:55 -0300

    feat: add repository interfaces

---
+++ IPedidoRepository.cs
@@ -0,0 +1,12 @@
+﻿using PedidosApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PedidosApp.Domain.Interfaces.Repositories
+{
+    public interface IPedidoRepository : IBaseRepository<Pedido, Guid>
+    {
+        List<Pedido> FindByCliente(Guid clienteId);
+    }
+}
